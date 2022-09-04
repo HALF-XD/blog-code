e76ba0c601026d2a4c5d1937e01f9850eaed9d8c
534925692 <534925692@qq.com>
2022-09-04 21:59:53 +0800

    -

---
+++ js-case-code/slideshow/case-2.js
@@ -0,0 +1,143 @@
+// https://zhuanlan.zhihu.com/p/151897269
+var autoplay = true;
+var autoplay_Delay = 2000; // ms
+var autoplayId;
+var intervalId;
+
+var slider;
+var slider_list;
+var slider_items;
+var indicator_container;
+
+var slider_item_width;
+var curIndex = 0;
+
+window.onload = function () {
+    initElement();
+    initEvent();
+    if (autoplay) {
+        startAnimation(slider_list);
+    }
+}
+
+function initElement() {
+    slider = document.getElementById("slider");
+    slider_list = slider.querySelector(".slider-list");
+    slider_items = slider_list.getElementsByClassName("slider-item");
+    indicator_container = slider.querySelector(".indicator-container");
+
+    let firstItem = slider_items[0].cloneNode(true); // 将第一张图片clone
+    slider_list.appendChild(firstItem); // 放在最后一张
+
+    slider_item_width = slider_items[0].offsetWidth; // 获取图片宽度
+}
+
+function initEvent() {
+    slider.addEventListener("mouseover", function () {
+        clearTimeout(autoplayId);
+        autoplay = false;
+    });
+
+    slider.addEventListener("mouseout", function () {
+        autoplay = true;
+        startAnimation(slider_list);
+    });
+
+    var indicators = indicator_container.children;
+    for (var i = 0; i < indicators.length; i++) {
+        indicators[i].setAttribute("index", i);
+        indicators[i].addEventListener("click", function () {
+            var index = parseInt(this.getAttribute("index"));
+            next(index);
+        });
+    }
+
+    var left_arrow = slider.getElementsByClassName("left-arrow")[0];
+    var right_arrow = slider.getElementsByClassName("right-arrow")[0];
+    left_arrow.addEventListener("click", function () {
+        prev();
+    });
+    right_arrow.addEventListener("click", function () {
+        next();
+    });
+}
+
+function animate(element, target) {
+    var step = 10;
+    var time = 10;
+    var gap = (Math.abs(target - element.offsetLeft) / slider_item_width);
+    if (gap > 1) {
+        step = step * gap;
+        time = time / gap;
+    }
+    if (element) {
+        step = (element.offsetLeft > target) ? -step : step;
+        clearInterval(intervalId);
+        setCurrentActiveIndicator(curIndex);
+        intervalId = setInterval(function () {
+            if ((step < 0) && (Math.abs(element.offsetLeft + step) < Math.abs(target))) {
+                element.style.left = element.offsetLeft + step + "px";
+            } else {
+                if (Math.abs(target - element.offsetLeft) > Math.abs(step)) {
+                    element.style.left = element.offsetLeft + step + "px";
+                } else {
+                    clearInterval(intervalId);
+                    intervalId = -1;
+                    element.style.left = target + "px";
+                    if (autoplay) {
+                        startAnimation(element);
+                    }
+                }
+            }
+        }, time);
+    }
+}
+
+function prev() {
+    var element = slider_list;
+    var li = element.children;
+    curIndex = curIndex - 1;
+    if (curIndex < 0) {
+        element.style.left = -((li.length - 1) * slider_item_width) + "px";
+        curIndex = li.length - 2;
+    }
+    animate(element, -(curIndex * slider_item_width));
+}
+
+function next(nextIndex) {
+    var element = slider_list;
+    var li = element.children;
+    if ((nextIndex != null) && (typeof (nextIndex) != "undefined")) {
+        curIndex = nextIndex;
+    } else {
+        curIndex = curIndex + 1;
+        if (curIndex > (li.length - 1)) {
+            element.style.left = 0 + "px";
+            curIndex = 1;
+        }
+    }
+    animate(element, -(curIndex * slider_item_width));
+}
+
+function startAnimation(element) {
+    if (autoplayId) {
+        clearTimeout(autoplayId);
+    }
+    autoplayId = setTimeout(function () {
+        next();
+    }, autoplay_Delay);
+}
+
+function setCurrentActiveIndicator(index) {
+    var indicators = indicator_container.children;
+    if (index == indicators.length) {
+        index = 0;
+    }
+    for (var i = 0; i < indicators.length; i++) {
+        if (i == index) {
+            indicators[i].className = "indicator active";
+        } else {
+            indicators[i].className = "indicator";
+        }
+    }
+}
